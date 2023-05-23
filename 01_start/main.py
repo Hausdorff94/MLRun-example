406a3ca8f54044798836beeb867e0d76acd7c35d
Johny Vallejo-Sánchez <35435216+Hausdorff94@users.noreply.github.com>
2023-05-23 20:24:46 +0000

    Run auto trainer

--- 01_start/main.py
+++ 01_start/main.py
@@ -16,3 +16,18 @@ if __name__ == "__main__":
     print(f"Generate data: {gen_data_run.state()}")
     print(f"Output: {gen_data_run.outputs}")
     print(gen_data_run.artifact("dataset").as_df().head())
+
+    # Import the function
+    trainer = mlrun.import_function("hub://auto_trainer")
+
+    trainer_run = project.run_function(
+        trainer,
+        inputs={"dataset": gen_data_run.outputs["dataset"]},
+        params={
+            "model_class": "sklearn.ensemble.RandomForestClassifier",
+            "train_test_split_size": 0.2,
+            "label_columns": "label",
+            "model_name": "cancer",
+        },
+        handler="train",
+    )
